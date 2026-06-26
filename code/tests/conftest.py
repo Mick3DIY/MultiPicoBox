389f145bcd699c3cbc5fa44728a6658e96694043
Michaël Mordefroy <michael.mordefroy@gmail.com>
2026-06-26 15:09:20 +0200

    Update unit tests

--- code/tests/conftest.py
+++ code/tests/conftest.py
@@ -10,25 +10,37 @@ from unittest.mock import MagicMock
 # Add parent 'tests' folder to PYTHONPATH
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
+# External mocks (from Adafruit classes)
+mock_board = MagicMock()
 mock_busio = MagicMock()
 mock_digitalio = MagicMock()
 mock_rotaryio = MagicMock()
 mock_mcp = MagicMock()
-
 mock_digitalio.Direction = MagicMock(INPUT="INPUT", OUTPUT="OUTPUT")
 mock_digitalio.Pull = MagicMock(UP="UP", DOWN="DOWN")
 
+sys.modules["board"] = mock_board
 sys.modules["busio"] = mock_busio
 sys.modules["digitalio"] = mock_digitalio
 sys.modules["rotaryio"] = mock_rotaryio
 sys.modules["adafruit_mcp230xx.mcp23017"] = mock_mcp
 
+# Internal mocks (from MultiPicoBoxV2 class)
+mock_encoder_mgr = MagicMock()
+mock_button_mgr = MagicMock()
+mock_mcp_mgr = MagicMock()
+
+sys.modules["encodermanager"] = mock_encoder_mgr
+sys.modules["buttonmanager"] = mock_button_mgr
+sys.modules["mcpmanager"] = mock_mcp_mgr
+
 
 @pytest.fixture(autouse=True)
 def clean_global_mocks():
     """Clean global mocks before each test"""
-    mock_busio.reset_mock()
-    mock_digitalio.reset_mock()
-    mock_rotaryio.reset_mock()
-    mock_mcp.reset_mock()
+    for mock in [
+        mock_board, mock_busio, mock_digitalio,
+        mock_rotaryio, mock_mcp, 
+        mock_encoder_mgr, mock_button_mgr, mock_mcp_mgr,]:
+        mock.reset_mock()
     mock_mcp.MCP23017.side_effect = None
--- code/tests/test_MultiPicoBoxV2.py
+++ code/tests/test_MultiPicoBoxV2.py
@@ -4,10 +4,75 @@
 # Unittest documentation : https://docs.python.org/3/library/unittest.html
 
 import pytest
+from unittest.mock import patch, MagicMock
 from MultiPicoBoxV2 import *
 from conftest import mock_busio, mock_digitalio, mock_mcp
 
+
 @pytest.fixture
-def manager():
+def my_box():
     """Fixture for each test"""
-    pass
+    # Internal mocks (from MultiPicoBoxV2 class)
+    from conftest import mock_encoder_mgr, mock_button_mgr, mock_mcp_mgr
+
+    def mock_init_encoder(pin1, pin2, pin3, name):
+        mock_instance = MagicMock()
+        mock_instance.get_name.return_value = name
+        return mock_instance
+
+    def mock_init_button(pin, name):
+        mock_instance = MagicMock()
+        mock_instance.get_name.return_value = name
+        return mock_instance
+
+    def mock_init_toggle(pin_1, pin_3, name):
+        mock_pin1 = mock_pin3 = MagicMock()
+        # Toggle switches are in Direction.INPUT and Pull.UP
+        mock_pin1.direction = mock_pin3.direction = "INPUT"
+        mock_pin1.pull = mock_pin3.pull = "UP"
+        return [mock_pin1, mock_pin3, name.upper()]
+
+    mock_encoder_mgr.EncoderManager.side_effect = mock_init_encoder
+    mock_button_mgr.ButtonManager.side_effect = mock_init_button
+    mock_mcp_instance = MagicMock()
+    mock_mcp_instance.get_toggle.side_effect = mock_init_toggle
+    mock_mcp_mgr.MCPManager.return_value = mock_mcp_instance
+    return MultiPicoBoxV2()
+
+
+def test_init_success(my_box):
+    """Test for the Encoder itself"""
+    # Check rotary encoders
+    assert len(my_box.rotary_encoders) == len(my_box.get_all_rot_encoders())
+    assert all(isinstance(item, EncoderManager) for item in my_box.get_all_rot_encoders())
+    assert "SW5" in my_box.rotary_encoders  # First
+    assert my_box.C_SW5 == "SW5"
+    assert "SW8" in my_box.rotary_encoders  # Last
+    assert my_box.C_SW8 == "SW8"
+    # Check push buttons
+    assert len(my_box.push_buttons) == len(my_box.get_all_push_buttons())
+    assert all(isinstance(item, ButtonManager) for item in my_box.get_all_push_buttons())
+    assert "SW9" in my_box.push_buttons  # First
+    assert my_box.C_SW9 == "SW9"
+    assert "SW12" in my_box.push_buttons  # Last
+    assert my_box.C_SW12 == "SW12"
+    # Check momentary switches
+    assert len(my_box.moment_switches) == len(my_box.get_all_mom_switches())
+    assert all(isinstance(item, ButtonManager) for item in my_box.get_all_mom_switches())
+    assert "SW13" in my_box.moment_switches  # First
+    assert my_box.C_SW13 == "SW13"
+    assert "SW16" in my_box.moment_switches  # Last
+    assert my_box.C_SW16 == "SW16"
+    # Check toggle switches (8 switches -> 4 toggles switches)
+    assert len(my_box.toggle_switches) / 2 == len(my_box.get_all_tog_switches())
+    assert "TSSW1_1" in my_box.toggle_switches  # First
+    assert my_box.C_TSSW1_1 == "TSSW1_1"
+    assert "TSSW4_1" in my_box.toggle_switches  # Last
+    assert my_box.C_TSSW4_1 == "TSSW4_1"
+    # Check <adafruit_mcp230xx.digital_inout.DigitalInOut> objects
+    for toggles in my_box.get_all_tog_switches():
+        for i in range(2):
+            assert toggles[i].direction == "INPUT"
+            assert toggles[i].pull == "UP"
+            # assert toggles[i].value == True
+        assert isinstance(toggles[2], str)
